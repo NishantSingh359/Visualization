ae547777882d6bfbfab49435ba93391d0a769626
Nishant Singh <singhnishant414243@gmail.com>
2026-02-11 10:06:18 +0530

    add more metrics

--- src/metrics.py
+++ src/metrics.py
@@ -24,12 +24,30 @@ class metrics:
             self.df.groupby("Country", as_index=False)["Revenue"]
             .sum()
             .sort_values("Revenue", ascending=False)
-        ) #type:ignore
-    
+        )  # type:ignore
+
     def orders_by_country(self):
         return (
             self.df.groupby("Country", as_index=False)["Revenue"]
             .count()
-            .rename(columns={'Revenue': 'Orders'}) #type:ignore
+            .rename(columns={"Revenue": "Orders"})  # type:ignore
             .sort_values("Orders", ascending=False)
-        ) 
+        )
+
+    def mountain_bikes_avg_price(self):
+        return (
+            self.df.groupby("Year", as_index=False)["Unit_Price"]
+            .mean()
+            .rename(columns={"Unit_Price": "Avg Price"})  # type:ignore
+            .sort_values("Year", ascending=True)
+        )
+
+    def product_by_revenue_and_profit(self):
+        return (
+            self.df.groupby("Product", as_index=False)[["Revenue", "Profit"]]
+            .sum()
+            .sort_values("Revenue", ascending=True)
+        )
+
+    def revenue_by_sub_category(self):
+        return self.df.groupby("Sub_Category", as_index=False)["Revenue"].sum()
