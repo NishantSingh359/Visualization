491c6d699563b7ef94b4be86b9dd8762fa5e24fb
Nishant Singh <singhnishant414243@gmail.com>
2026-02-11 10:17:14 +0530

    add configration

--- src/plot/base/plotbase.py
+++ src/plot/base/plotbase.py
@@ -1,6 +1,8 @@
+import yaml
+import numpy as np
 import matplotlib.pyplot as plt
 from abc import ABC, abstractmethod
-import src.common as comm
+import plot.common.common as comm
 from matplotlib.ticker import FuncFormatter
 
 formatter = FuncFormatter(comm.chart_format_number)
@@ -16,6 +18,10 @@ class PlotBase:
         title="Plot",
         xlabel="xlabel",
         ylabel="ylabel",
+        xvlinevalue=np.nan,
+        xhlinevalue=np.nan,
+        yaxisformatter=True,
+        xaxisformatter=False,
         fsize=(6, 4),
     ):
         self.x = x
@@ -25,27 +31,114 @@ class PlotBase:
         self.title = title
         self.xlabel = xlabel
         self.ylabel = ylabel
+        self.xvlinevalue = xvlinevalue
+        self.xhlinevalue = xhlinevalue
+        self.xaxisformatter = xaxisformatter
+        self.yaxisformatter = yaxisformatter
         self.fsize = fsize
 
     def base(self):
+
+        font = self.config()
+        title = self.config()["title"]
+        xaxis = self.config()["axis"]["xaxis"]
+        yaxis = self.config()["axis"]["yaxis"]
+        tick = self.config()["axis"]["tick"]
+        vline = self.config()["line"]["vline"]
+        hline = self.config()["line"]["hline"]
+
         plt.figure(figsize=self.fsize)
         ax = plt.gca()
         ax.set_axisbelow(True)
+
         self.plot(self.x, self.y1, self.y2, self.y3)
         plt.grid(True, axis="y", color="gray", alpha=0.5)
-        plt.title(self.title, fontdict=dict(size=22, color="gray"), loc="left", pad=30)
+
+        plt.title(
+            self.title,
+            loc=title["loc"],
+            pad=title["pad"],
+            fontdict=dict(
+                size=title["size"],
+                color=title["color"],
+                weight=title["weight"],
+                style=title["style"],
+                fontname=font["fontname"],
+            ),
+        )
+
         plt.gca().spines["top"].set_visible(False)
         plt.gca().spines["right"].set_visible(False)
         plt.gca().spines["left"].set_color("gray")
         plt.gca().spines["bottom"].set_color("gray")
-        plt.gca().yaxis.set_major_formatter(formatter)
-        plt.xlabel(self.xlabel, fontdict=dict(size=15, color="gray"))
-        plt.ylabel(self.ylabel, fontdict=dict(size=15, color="gray"))
-        plt.tick_params(color="gray")
 
-        plt.xticks(size=12, rotation=90, color="gray")
-        plt.yticks(size=12, color="gray")
+        if self.yaxisformatter:
+            plt.gca().yaxis.set_major_formatter(formatter)
+
+        if self.xaxisformatter:
+            plt.gca().xaxis.set_major_formatter(formatter)
+
+        plt.xlabel(
+            self.xlabel,
+            fontdict=dict(
+                size=xaxis["label"]["size"],
+                color=xaxis["label"]["color"],
+                weight=xaxis["label"]["weight"],
+                style=xaxis["label"]["style"],
+                fontname=font["fontname"]
+            )
+        )
+        plt.ylabel(
+            self.ylabel,
+            fontdict=dict(
+                size=xaxis["label"]["size"],
+                color=xaxis["label"]["color"],
+                weight=xaxis["label"]["weight"],
+                style=xaxis["label"]["style"],
+                fontname=font["fontname"]
+            )
+        )
+
+        plt.xticks(
+            size=xaxis["ticks"]["size"],
+            color=xaxis["ticks"]["color"],
+            weight=xaxis['ticks']['weight'],
+            style=xaxis['ticks']['style'],
+            fontname=font["fontname"],
+            rotation=xaxis["ticks"]["rotation"],
+        )
+        plt.yticks(
+            size=yaxis["ticks"]["size"],
+            color=yaxis["ticks"]["color"],
+            weight=yaxis['ticks']['weight'],
+            style=yaxis['ticks']['style'],
+            fontname=font["fontname"],
+            rotation=yaxis["ticks"]["rotation"],
+        )
+
+        plt.tick_params(size=tick["size"], color=tick["color"])
+
+        plt.axvline(
+            self.xvlinevalue,
+            color=vline["color"],
+            alpha=vline["alpha"],
+            linestyle=vline["style"],
+            linewidth=vline["width"],
+        )
+        plt.axhline(
+            self.xhlinevalue,
+            color=hline["color"],
+            alpha=hline["alpha"],
+            linestyle=hline["style"],
+            linewidth=hline["width"],
+        )
+
         plt.savefig(f"output/{self.title}.jpg", dpi=300, bbox_inches="tight")
+
+    def config(self):
+        with open("src\\plot\\config\\decorator.yaml") as f:
+            cfg = yaml.full_load(f)["common"]
+        return cfg
 
     @abstractmethod
     def plot(self, x, y1, y2, y3):
