d3014bcfb28f232fc3b63a8c9b330f221e691d2b
Nishant Singh <singhnishant414243@gmail.com>
2026-02-11 10:06:48 +0530

    add more plot

--- src/run.py
+++ src/run.py
@@ -1,15 +1,18 @@
 import importlib
 import pandas as pd
 import src.metrics as m
-import plot.chart.lineplot as li
-import plot.chart.columnplot as co
-import plot.chart.areaplot as ar
+import src.plot.chart.lineplot as li
+import src.plot.chart.columnplot as co
+import src.plot.chart.areaplot as ar
+import src.plot.chart.stepplot as st
+import src.plot.chart.scatterplot as sc
 
 importlib.reload(li)
 importlib.reload(co)
 importlib.reload(ar)
 importlib.reload(m)
-
+importlib.reload(st)
+importlib.reload(sc)
 
 df = pd.read_csv(
     r"C:\Users\TUF\OneDrive\Documents\Code\Vs Code\Visualization\data\Bike Sales.csv"
@@ -38,10 +41,45 @@ li.LinePlot(
 
 data = mt.revenue_by_country()
 ar.AreaPlot(
-    x=data['Country'],
-    y1=data['Revenue'],
+    x=data["Country"],
+    y1=data["Revenue"],
     title="Revenue by Country",
     xlabel="Country",
     ylabel="Revenue",
-    fsize=(10, 6),    
+    fsize=(10, 6),
+).base()
+
+data = mt.mountain_bikes_avg_price()
+st.StepPlot(
+    x=data["Year"],
+    y1=data["Avg Price"],
+    title="Mountain Bike's Avg Price",
+    xlabel="Year",
+    ylabel="Avg Price",
+    fsize=(10, 6),
+).base()
+
+
+data = mt.product_by_revenue_and_profit()
+sc.ScatterPlot(
+    x=data["Revenue"],
+    y1=data["Profit"],
+    title="Product by Revenue & Profit",
+    xlabel="Revenue",
+    ylabel="Profit",
+    xvlinevalue=data["Revenue"].mean(),
+    xhlinevalue=data["Profit"].mean(),
+    xaxisformatter=True,
+    fsize=(10, 6),
+).base()
+
+
+data = mt.revenue_by_sub_category()
+sc.ScatterPlot(
+    x=data["Sub_Category"],
+    y1=data["Revenue"],
+    title="Revenue by Sub-Category",
+    xlabel="Sub-Category",
+    ylabel="Revenue",
+    fsize=(10, 6),
 ).base()
